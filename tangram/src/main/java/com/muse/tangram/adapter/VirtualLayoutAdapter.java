c6a7a3c22e2daf016d5dc98b0996da037afbf2a8
Guowee <guoweegw@gmail.com>
2018-03-05 11:34:38 +0800

    update package name

---
+++ tangram/src/main/java/com/muse/tangram/adapter/VirtualLayoutAdapter.java
@@ -0,0 +1,36 @@
+package com.muse.tangram.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import com.muse.tangram.VirtualLayoutManager;
+import com.muse.tangram.helper.LayoutHelper;
+
+import java.util.List;
+
+/**
+ * Created by GuoWee on 2018/1/13.
+ */
+
+public abstract class VirtualLayoutAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
+
+    protected VirtualLayoutManager mLayoutManager;
+
+    public VirtualLayoutAdapter(VirtualLayoutManager manager) {
+        this.mLayoutManager = manager;
+    }
+
+    /**
+     * 设置整个页面所需要的一系列LayoutHelper
+     */
+    public void setLayoutHelpers(List<LayoutHelper> helpers) {
+        mLayoutManager.setLayoutHelpers(helpers);
+    }
+
+    @NonNull
+    public List<LayoutHelper> getLayoutHelpers() {
+        return this.mLayoutManager.getLayoutHelpers();
+    }
+
+
+}
